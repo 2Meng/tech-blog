0b100e9a7b21277dfa01caeb952ff8b01310c2ee
2Meng <tyan1401@gmail.com>
2023-08-04 11:39:42 -0500

    bug in profile but pushing

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,26 +1,18 @@
 const router = require('express').Router();
-const { Comment, User, Blog } = require('../models');
-const withAuth = require('../utils/auth');
+const { Blog, User, Comment } = require('../models');
 
 router.get('/', async (req, res) => {
   try {
-    // Get all projects and JOIN with user data
+    // Get all blogs and JOIN with user data
     const blogData = await Blog.findAll({
-      include: [
-        {
-          model: User,
-        },
-        {
-          model: Comment
-        }
-      ],
+      include: [{ model: User }, { model: Comment }],
     });
 
     // Serialize data so the template can read it
     const blogs = blogData.map((blog) => blog.get({ plain: true }));
 
-    // Pass serialized data and session flag into template
-    res.render('homepage', { 
+    // Pass serialized data 
+    res.render('homepage', {
       blogs
     });
   } catch (err) {
@@ -28,44 +20,19 @@ router.get('/', async (req, res) => {
   }
 });
 
-// Use withAuth middleware to prevent access to route
-router.get('/profile', withAuth, async (req, res) => {
-  try {
-    // Find the logged in user based on the session ID
-    const userData = await User.findByPk(req.session.user_id, {
-      attributes: { exclude: ['password'] },
-      include: [{ model: Project }],
-    });
-
-    const user = userData.get({ plain: true });
-
-    res.render('profile', {
-      ...user,
-      logged_in: true
-    });
-  } catch (err) {
-    res.status(500).json(err);
-  }
-});
-
 router.get('/login', async (req, res) => {
-  // If the user is already logged in, redirect the request to another route
-  if (req.session.logged_in) {
-    res.redirect('/profile');
-    return;
-  }
   try {
     res.render('login');
-  } catch (error) {
-    res.status(500).json(error);
+  } catch (err) {
+    res.status(500).json(err);
   }
 });
 
 router.get('/logout', async (req, res) => {
   try {
     res.render('logout');
-  } catch (error) {
-    res.status(500).json(error);
+  } catch (err) {
+    res.status(500).json(err);
   }
 });
 
--- seeds/seed.js
+++ seeds/seed.js
@@ -1,8 +1,9 @@
 const sequelize = require('../config/connection');
-const { User, Project } = require('../models');
+const { User, Blog, Comment } = require('../models');
 
 const userData = require('./userData.json');
-const projectData = require('./projectData.json');
+const blogData = require('./blogData.json');
+const commentData = require('./commentData.json');
 
 const seedDatabase = async () => {
   await sequelize.sync({ force: true });
@@ -12,12 +13,21 @@ const seedDatabase = async () => {
     returning: true,
   });
 
-  for (const project of projectData) {
-    await Project.create({
-      ...project,
-      user_id: users[Math.floor(Math.random() * users.length)].id,
+// Create blogs
+for (const blog of blogData) {
+  const createdBlog = await Blog.create({
+    ...blog,
+    user_id: users[Math.floor(Math.random() * users.length)].id,
+  });
+
+  // Create comments for current blog
+  for (const comment of commentData) {
+    await Comment.create({
+      ...comment,
+      blog_id: createdBlog.id,
     });
   }
+}
 
   process.exit(0);
 };
